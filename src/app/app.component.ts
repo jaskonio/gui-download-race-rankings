a3a47e452e467b1ae9afb60f2c54b5d62f87a958
Jonatan <jonathan.lp17@gmail.com>
2023-03-05 21:35:51 +0000

    Added first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import csvDownload from 'json-to-csv-export'
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gui-download-race-rankings';
+
+  urlFormControl = new FormControl('', [Validators.required]);
+
+  dataToConvert = {
+    data: [],
+    filename: 'runner_data',
+    delimiter: ',',
+    headers: ["id","title","description","price","discountPercentage","rating","stock","brand","category","thumbnail","images"]
+  }
+
+  constructor(private http:HttpClient) {
+  }
+  
+  downloadData() {  
+    console.log("value: " + this.urlFormControl.value)
+
+    if( this.urlFormControl.value == "") {
+      // set error
+      return
+    }
+
+    this.http.get("https://dummyjson.com/products").subscribe((response: any) => {
+      console.log(response['products'])
+      this.dataToConvert.data = response['products']
+
+      console.log(this.dataToConvert)
+      csvDownload(this.dataToConvert)
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+
+import {MatButtonModule} from '@angular/material/button';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
